7529e27250c526a5469da042d8f99f1f454ee5af
SavitTumuluri <savit.s.tumuluri@gmail.com>
2026-05-21 23:09:30 -0400

    dialogue progress

--- Assets/Scripts/ImageCleaner/transparency.py
+++ Assets/Scripts/ImageCleaner/transparency.py
@@ -1,8 +1,8 @@
 from PIL import Image
 from collections import deque
 
-input_file = "resume_button_bg.png"
-output_file = "resume_button.png"
+input_file = "ren_smiling_bg.png"
+output_file = "ren_smiling.png"
 
 img = Image.open(input_file).convert("RGBA")
 pixels = img.load()
